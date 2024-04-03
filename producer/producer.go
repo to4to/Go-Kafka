be31f6551231240d9fbd72db8f630e4e3848b248
to4to <justjustifying@outlook.com>
2024-04-03 23:08:24 +0530

    Add fiber framework and API route

--- producer/producer.go
+++ producer/producer.go
@@ -1,5 +1,11 @@
 package main
+import "github.com/gofiber/fiber/v2"
 
 type Comment struct {
 	Text string `form:"text" json:"text"`
+}
+
+func main() {
+app:=fiber.New();
+api:=app.Group("/api/v1")
 }
