793a98d6ea75c0ffe880688eb118cc2b03ca14f2
to4to <justjustifying@outlook.com>
2024-04-04 01:50:29 +0530

    Add createComment endpoint to API

--- producer/producer.go
+++ producer/producer.go
@@ -8,4 +8,11 @@ type Comment struct {
 func main() {
 app:=fiber.New();
 api:=app.Group("/api/v1")
+api.Post("/comments",createComment)
+
+app.Listen(":3000")
+}
+
+func createComment(c *fiber.Ctx) error{
+	
 }
